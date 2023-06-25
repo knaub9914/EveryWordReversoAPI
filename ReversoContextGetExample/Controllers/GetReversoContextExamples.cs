f6d998dc67b87349e1f7165768e5396a5c51a92d
KonstantinK <konstantin@loadlink.ca>
2023-06-24 23:45:51 -0400

    first commit

---
+++ ReversoContextGetExample/Controllers/GetReversoContextExamples.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ReversoAPI;
+using ReversoContextGetExample.Models;
+
+namespace ReversoContextGetExample.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GetReversoContextExamples : ControllerBase
+{
+    private readonly ILogger<GetReversoContextExamples> _logger;
+
+    public GetReversoContextExamples(ILogger<GetReversoContextExamples> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetContextExamples")]
+    public async Task<List<ReversoContextExample>> GetContextExamplesBySourceLanguage(
+        string Word, string SourceLanguage, string TargetLanguage) {
+
+        var reverso = new ReversoClient();
+        ContextData context = new ContextData();
+        List<ReversoContextExample> exampleSentences = new List<ReversoContextExample>();
+        Language sourceLanguage = Enum.Parse<ReversoAPI.Language>(SourceLanguage);
+        Language targetLanguage = Enum.Parse<ReversoAPI.Language>(TargetLanguage);
+        try
+        {
+            context = await reverso.Context.GetAsync(Word, sourceLanguage, targetLanguage);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+        }
+
+        var examples = context.Examples.ToList();
+
+        for (int i = 0; i < examples.Count(); i++)
+        {
+            exampleSentences.Add(new ReversoContextExample { Content = examples[i].Source.Text.ToString(),
+                Id = Guid.NewGuid(),SourceLanguage = SourceLanguage });
+        }
+        return exampleSentences;
+    }
+}
+
---
+++ ReversoContextGetExample/Models/ReversoContextExample.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ReversoContextGetExample.Models
+{
+	public class ReversoContextExample
+	{
+        public Guid Id { get; set; }
+		public string? SourceLanguage { get; set; }
+		public string? Content { get; set; }
+	}
+}
+
